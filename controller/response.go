62e27b21293791a1043b0a5a54a871634874ff90
North <45125250+Northes@users.noreply.github.com>
2021-06-02 22:20:19 +0800

    feat(response.go): 新增错误码以及响应函数

--- controller/response.go
+++ controller/response.go
@@ -6,32 +6,50 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type ResponseCode int
+
+const (
+	CodeSuccess          ResponseCode = 200
+	CodeInvalidParameter ResponseCode = 0
+	CodeServerBusy       ResponseCode = 1
+)
+
+var responseCodeMap = map[ResponseCode]string{
+	CodeSuccess:          "成功",
+	CodeInvalidParameter: "参数错误",
+	CodeServerBusy:       "服务繁忙",
+}
+
 type response struct {
-	Code int         `json:"code"`
-	Msg  string      `json:"msg"`
-	Data interface{} `json:"data,omitempty"`
+	Code ResponseCode `json:"code"`
+	Msg  string       `json:"msg"`
+	Data interface{}  `json:"data,omitempty"`
+}
+
+func (c ResponseCode) Msg() string {
+	return responseCodeMap[c]
 }
 
 func ResponseSuccess(c *gin.Context, data interface{}) {
 	c.JSON(http.StatusOK, &response{
-		Code: 200,
-		Msg:  "success",
+		Code: CodeSuccess,
+		Msg:  CodeSuccess.Msg(),
 		Data: data,
 	})
 }
 
 func ResponseSuccessWithMsg(c *gin.Context, msg string, data interface{}) {
 	c.JSON(http.StatusOK, &response{
-		Code: 200,
+		Code: CodeSuccess,
 		Msg:  msg,
 		Data: data,
 	})
 }
 
-func ResponseError(c *gin.Context) {
+func ResponseError(c *gin.Context, code ResponseCode) {
 	c.JSON(http.StatusOK, &response{
-		Code: 300,
-		Msg:  "错误",
+		Code: code,
+		Msg:  code.Msg(),
 		Data: nil,
 	})
 }
