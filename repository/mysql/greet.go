97f0865c72d5cd95f416a76bebfbdf7388a77af1
North <45125250+Northes@users.noreply.github.com>
2021-06-07 16:12:18 +0800

    fix(mysql\greet): 修复取得的结果与预期严重不符的问题

--- repository/mysql/greet.go
+++ repository/mysql/greet.go
@@ -7,7 +7,7 @@ import (
 func GetGreet(g *model.Greet) (greetList []model.Greet, err error) {
 	greetList = make([]model.Greet, 0)
 	err = engine.Where("time=?", g.Time).
-		Or("day=?", g.Day).
+		And("day=?", g.Day).
 		Find(&greetList)
 	if err != nil {
 		return nil, err
