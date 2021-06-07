68b0e0f666c4924e82255afa78db1b97e16dac03
North <45125250+Northes@users.noreply.github.com>
2021-06-07 23:40:29 +0800

    feat(router): 更新路由

--- router/router.go
+++ router/router.go
@@ -14,11 +14,14 @@ func Setup() *gin.Engine {
 	r.Use(cors.Default())
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Home")
+		c.JSON(http.StatusOK, gin.H{
+			"Docs": "https://docs.apihut.net/",
+		})
 	})
 	r.GET("/greet", controller.GreetHandler)
 	r.GET("/weather", controller.WeatherHandler)
 	r.GET("/ip", controller.IPHandler)
+	r.GET("/words", controller.WordsHandler)
 
 	return r
 }
