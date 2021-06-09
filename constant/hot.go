2930c521d85e3ffeb9a05509d9ef2e78285d25d1
North <45125250+Northes@users.noreply.github.com>
2021-06-09 11:31:32 +0800

    feat(constant): 新增热榜网址常量

---
+++ constant/hot.go
@@ -0,0 +1,9 @@
+package constant
+
+const (
+	BaiduHotUrl = "http://top.baidu.com/buzz?b=1&fr=topindex"
+	//BaiduHotUrl = "http://baidu.apihut.net/"
+	SinaHotUrl = "https://s.weibo.com/top/summary?cate=realtimehot"
+	//SinaHotUrl = "http://sina.apihut.net/"
+	SinaHrefUrl = "https://s.weibo.com"
+)
