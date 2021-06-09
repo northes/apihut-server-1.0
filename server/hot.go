eacb790f5ea3c135ac980970a2c0eb5c408e7ca5
North <45125250+Northes@users.noreply.github.com>
2021-06-09 12:38:05 +0800

    style(hot): 完善注释

--- server/hot.go
+++ server/hot.go
@@ -25,21 +25,20 @@ func GetHot(site string) ([]model.HotItem, error) {
 
 	switch site {
 	case constant.SiteNameBaidu:
-		return GetBaiduHot(&proxyIP)
+		return getBaiduHot(&proxyIP)
 	case constant.SiteNameSina:
-		return GetSinaHot(&proxyIP)
+		return getSinaHot(&proxyIP)
 	case constant.SiteNameThePaper:
-		return GetThePaperHot(&proxyIP)
+		return getThePaperHot(&proxyIP)
 	default:
-		return GetBaiduHot(&proxyIP)
+		return getBaiduHot(&proxyIP)
 	}
-
-	//return GetSinaHot(&proxyIP)
 }
 
-func GetBaiduHot(proxyIP *string) (hotList []model.HotItem, err error) {
+// 获取百度热榜
+func getBaiduHot(proxyIP *string) (hotList []model.HotItem, err error) {
 
-	c := GetColly(proxyIP)
+	c := getColly(proxyIP)
 
 	c.OnHTML(".list-table tr:not(.item-tr)", func(e *colly.HTMLElement) {
 		title, _ := simplifiedchinese.GBK.NewDecoder().Bytes([]byte(string(e.ChildText(".keyword .list-title"))))
@@ -80,8 +79,9 @@ func GetBaiduHot(proxyIP *string) (hotList []model.HotItem, err error) {
 	return hotList[1:], err
 }
 
-func GetSinaHot(proxyIP *string) (hotList []model.HotItem, err error) {
-	c := GetColly(proxyIP)
+// 获取微博热搜
+func getSinaHot(proxyIP *string) (hotList []model.HotItem, err error) {
+	c := getColly(proxyIP)
 
 	c.OnHTML("tbody tr ", func(e *colly.HTMLElement) {
 		title := e.ChildText(".td-02 a")
@@ -110,9 +110,10 @@ func GetSinaHot(proxyIP *string) (hotList []model.HotItem, err error) {
 	return hotList, err
 }
 
-func GetThePaperHot(proxyIP *string) (hotList []model.HotItem, err error) {
+// 获取澎湃新闻热闻
+func getThePaperHot(proxyIP *string) (hotList []model.HotItem, err error) {
 
-	c := GetColly(proxyIP)
+	c := getColly(proxyIP)
 
 	c.OnHTML("#listhot0 li:not(.list_more)", func(e *colly.HTMLElement) {
 		title := e.ChildText("a")
@@ -129,8 +130,8 @@ func GetThePaperHot(proxyIP *string) (hotList []model.HotItem, err error) {
 	return hotList, nil
 }
 
-// GetColly 返回Colly收集器
-func GetColly(proxyIP *string) *colly.Collector {
+// 返回Colly收集器
+func getColly(proxyIP *string) *colly.Collector {
 	c := colly.NewCollector(
 		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"),
 		colly.MaxDepth(1),
