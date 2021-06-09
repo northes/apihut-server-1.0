aec7d2fd5b55db3de788058e9cbc349bdeafc603
North <45125250+Northes@users.noreply.github.com>
2021-06-09 11:35:56 +0800

    feat(constant): 新增热榜网站名常量

--- constant/hot.go
+++ constant/hot.go
@@ -1,6 +1,11 @@
 package constant
 
 const (
+	SiteNameBaidu = "baidu"
+	SiteNameSina  = "sina"
+)
+
+const (
 	BaiduHotUrl = "http://top.baidu.com/buzz?b=1&fr=topindex"
 	//BaiduHotUrl = "http://baidu.apihut.net/"
 	SinaHotUrl = "https://s.weibo.com/top/summary?cate=realtimehot"
