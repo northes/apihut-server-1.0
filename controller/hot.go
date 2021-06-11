a5560b5b92812121e2fb8b216b10b478012d55d5
North <45125250+Northes@users.noreply.github.com>
2021-06-11 20:42:56 +0800

    refactor(hot): 优化路由参数验证

--- controller/hot.go
+++ controller/hot.go
@@ -9,21 +9,12 @@ import (
 )
 
 func RankHandler(c *gin.Context) {
-	siteName := c.Param("site")
+	siteName := constant.SiteName(c.Param("site"))
 
 	// 参数检查
-	if siteName != constant.SiteNameBaidu &&
-		siteName != constant.SiteNameSina &&
-		siteName != constant.SiteNameZhihu &&
-		siteName != constant.SiteNameThePaper &&
-		siteName != constant.SiteNameBilibili &&
-		siteName != constant.SiteNameBilibiliShort &&
-		siteName != constant.SiteNameITHome {
+	if !siteName.IsSiteName() {
 		ResponseError(c, CodeParameterFailure)
 		return
-	}
-	if siteName == constant.SiteNameBilibiliShort {
-		siteName = constant.SiteNameBilibili
 	}
 
 	rank, err := server.GetRank(siteName)
