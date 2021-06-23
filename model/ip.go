f2d7799332804b00361e259d9ba362924d703ceb
North <45125250+Northes@users.noreply.github.com>
2021-06-23 09:18:40 +0800

    feat(ip): 表加入创建时间、更新时间字段

--- model/ip.go
+++ model/ip.go
@@ -1,14 +1,18 @@
 package model
 
+import "time"
+
 type IP struct {
-	Status   string `json:"status,omitempty" xorm:"-"`
-	Info     string `json:"info,omitempty" xorm:"-"`
-	Infocode string `json:"infocode,omitempty" xorm:"-"`
-	IP       string `json:"ip" xorm:"index notnull comment(IP)"`
-	Country  string `json:"country"  xorm:"comment(国家)"`
-	Province string `json:"province" xorm:"comment(省份)"`
-	City     string `json:"city" xorm:"comment(城市)"`
-	District string `json:"district" xorm:"comment(地区)"`
-	ISP      string `json:"isp" xorm:"comment(运营商)"`
-	Location string `json:"location" xorm:"comment(经纬度)"`
+	Status    string    `json:"status,omitempty" xorm:"-"`
+	Info      string    `json:"info,omitempty" xorm:"-"`
+	Infocode  string    `json:"infocode,omitempty" xorm:"-"`
+	IP        string    `json:"ip" xorm:"index notnull comment(IP)"`
+	Country   string    `json:"country"  xorm:"comment(国家)"`
+	Province  string    `json:"province" xorm:"comment(省份)"`
+	City      string    `json:"city" xorm:"comment(城市)"`
+	District  string    `json:"district" xorm:"comment(地区)"`
+	ISP       string    `json:"isp" xorm:"comment(运营商)"`
+	Location  string    `json:"location" xorm:"comment(经纬度)"`
+	CreatedAt time.Time `json:"-" xorm:"created comment(创建时间)"`
+	UpdatedAt time.Time `json:"-" xorm:"updated comment(更新时间)"`
 }
